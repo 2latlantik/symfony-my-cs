4f75d3acb29209d8eaa389624a49945cd6321924
Nicolas <nicolascoudenne@gmail.com>
2018-03-30 23:34:40 +0200

    Next step

---
+++ src/Git/Hooks/PostInstall.php
@@ -0,0 +1,12 @@
+<?php 
+namespace SymfonyMyCs\Git\Hooks;
+
+use Composer\Script\Event;
+
+class PostInstall 
+{
+    public static function installHooks(Event $event) {
+        $io = $event->getIO();
+        $io->write('<info>installHooks!</info>');
+    }
+}
